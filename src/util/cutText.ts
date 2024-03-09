b96804390beb0ebb3e14502fa2627f4631c36527
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-03-09 21:50:30 +0000

    update

--- src/util/cutText.ts
+++ src/util/cutText.ts
@@ -1,11 +1,5 @@
-export const cutText = (
-	text: string,
-	maxLength: number,
-	addDots = true
-): string => {
+export const cutText = (text: string, maxLength: number, addDots = true): string => {
 	return text.length > maxLength
-		? `${text.slice(0, addDots ? maxLength - 3 : maxLength)}${
-				addDots ? '...' : ''
-		  }`
+		? `${text.slice(0, addDots ? maxLength - 3 : maxLength)}${addDots ? '...' : ''}`
 		: text;
 };
