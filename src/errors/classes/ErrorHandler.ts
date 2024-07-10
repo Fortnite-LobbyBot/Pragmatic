b3b3d78310c10c3b319fe378b27573bd8a848edf
tnfAngel <57068341+tnfAngel@users.noreply.github.com>
2024-07-10 10:37:20 +0100

    some fixes

--- src/errors/classes/ErrorHandler.ts
+++ src/errors/classes/ErrorHandler.ts
@@ -1,8 +1,8 @@
 import type { ErrorCodes } from '../../types/enums/error/ErrorCodes';
-import { type errorMap, validatedErrorMap } from '../../types/enums/error/ErrorMap';
+import { errorMap } from '../../types/enums/error/ErrorMap';
 
 export abstract class ErrorHandler {
 	public static getError<T extends ErrorCodes>(error: T): (typeof errorMap)[T] {
-		return validatedErrorMap[error];
+		return errorMap[error];
 	}
 }
